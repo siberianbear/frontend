b351e7bd9c23ec33a2f8bc2f7b713ba42244915e
Akash A <akash1810@gmail.com>
2016-05-24 20:59:10 +0100

    wrapping in ab test

--- static/src/javascripts/projects/common/modules/experiments/tests/video-teaser.js
+++ static/src/javascripts/projects/common/modules/experiments/tests/video-teaser.js
@@ -27,11 +27,11 @@ define([
 
         this.variants = [
             {
-                id: 'baseline1',
+                id: 'control',
                 test: function () {}
             },
             {
-                id: 'baseline2',
+                id: 'variant',
                 test: function () {}
             }
         ];
